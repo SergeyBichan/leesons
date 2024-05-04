b99e7c075aa9ffe6618759f8920f33b688355004
SergeyBichan <ugidancer@gmail.com>
2024-05-04 18:31:03 +0300

    bubble sort

---
+++ src/lesson_3/task1/bubblesort/Runner.java
@@ -0,0 +1,55 @@
+package lesson_3.task1.bubblesort;
+
+public class Runner {
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Unsorted Array:");
+        printArray(array);
+
+        bubbleSort(array);
+        //проверка на отсортированность
+        bubbleSort(array);
+
+        System.out.println("\nSorted Array:");
+        printArray(array);
+
+    }
+
+    // Метод для сортировки массива пузырьком
+    public static void bubbleSort(int[] array) {
+        if (checkIfSorted(array)) {
+            System.out.println("Massive is already sorted!");
+        } else {
+            int n = array.length;
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = 0; j < n - i - 1; j++) {
+                    if (array[j] > array[j + 1]) {
+                        // Меняем местами элементы
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    // Метод для вывода массива
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // Метод для проверки, отсортирован ли массив
+    public static boolean checkIfSorted(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
